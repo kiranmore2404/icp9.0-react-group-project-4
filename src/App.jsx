28322b7c413303560321009333ad228f5367b0a9
YogitaBhujadi <yogitabhujadi6@gmail.com>
2025-02-18 10:26:43 +0530

    form add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar"; 
 import Home from "./views/Home/Home";
 import TrainExplorer from "./views/TrainSearch/TrainExplorer";
+import Booking from "./views/Booking/Booking";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/train-explorer" element={<TrainExplorer />} />
+        <Route path="/booking" element={<Booking />} />
       </Routes>
       <Footer />
     </Router>
