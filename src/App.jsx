00ff22a7592a19419082ba30b85cb8849a8d110c
Vrush2004 <vrushalichaube@gmail.com>
2025-02-18 11:53:14 +0530

    About page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar"; 
 import Home from "./views/Home/Home";
 import TrainExplorer from "./views/TrainSearch/TrainExplorer";
-
+import TrainSchedule from "./views/TrainSchedule/TrainSchedule";
+import Booking from "./views/Booking/Booking";
+import PassengerDetails from "./views/Booking/PassengerDetail";
+import Payment from "./views/Booking/Payment";
+import About from './views/About/About';
 
 const App = () => {
   return (
@@ -13,6 +17,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/train-explorer" element={<TrainExplorer />} />
+        <Route path="/train-schedules" element={<TrainSchedule />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/passenger-details" element={<PassengerDetails />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </Router>
